fb4dd796abeade1f2ec2b76b7049b677e7886a09
Jashele T <jashele@Jasheles-Air.domain>
2019-09-25 11:30:31 -0700

    Updates

---
+++ precache-manifest.350a596a71917e0a99f83d45dc995d02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44296cc83aed67b5ef90010a8755f6d0",
+    "url": "/Car-Sales/index.html"
+  },
+  {
+    "revision": "58b6723899fb97654331",
+    "url": "/Car-Sales/static/css/2.1ab5750d.chunk.css"
+  },
+  {
+    "revision": "22ef5d5de6961a87faff",
+    "url": "/Car-Sales/static/css/main.ee2cb2aa.chunk.css"
+  },
+  {
+    "revision": "58b6723899fb97654331",
+    "url": "/Car-Sales/static/js/2.aadb8efa.chunk.js"
+  },
+  {
+    "revision": "22ef5d5de6961a87faff",
+    "url": "/Car-Sales/static/js/main.0232e8c9.chunk.js"
+  },
+  {
+    "revision": "1f9dcd49ef87d4733a65",
+    "url": "/Car-Sales/static/js/runtime~main.94ca7d13.js"
+  }
+]);
